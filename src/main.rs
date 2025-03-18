f905aa0e29559c723e26f2530957464e86a25632
kira malinova <llathasa@outlook.com>
2025-03-18 09:25:48 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,48 @@
-use ureq::{get, Error};
-use xmlparser::{Token, Tokenizer};
+use anyhow::{Context, Result};
+use serde::Deserialize;
 
-fn main() -> Result<(), Error> {
-    let uri: String = ["https://api.forismatic.com/api/1.0/?",
-                       "method=getQuote&format=xml&lang=en"].join("");
-    match get(&uri).call() {
-        Ok(response) => {
-            let text = response.into_string()?;
-            process_response(text);
-            Ok(())
-        }
-        Err(Error::Status(code, _response)) => {
-            println!("\x1B[94m\x1B[1mHTTP error:\x1B[0m \x1B[91m{}", code);
-            Ok(()) // no error if http error
-        }
-        Err(e) => {
-            println!("\x1B[91m\x1B[1mUnknown error.");
-            Err(e) // error if something bad happened...
-        }
-    }
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Quote {
+    quote_text: String,
+    quote_author: String,
 }
 
-fn process_response(text: String) {
-    let data = Tokenizer::from(text.as_str())
-        .fold(Vec::new(), |mut res, t| {
-            if let Ok(token) = t {
-                if let Token::Text { text } = token {
-                    res.push(text.as_str().trim());
-                }
-            }
-            res
-        });
-    println!(
-        "\"\x1B[94m\x1B[1m{}\x1B[0m\"",
-        textwrap::fill(data[0], 60).replace("\n", "\n "),
-    );
-    if data.len() == 3 {
-        println!("\x1B[93m{}\x1B[0m", data[1]);
+const YELLOW: &str = "\x1B[93m";
+const BLUE: &str = "\x1B[94m";
+const BOLD: &str = "\x1B[1m";
+const RESET: &str = "\x1B[0m";
+
+fn main() -> Result<()> {
+    let uri = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
+
+    let response = get_request(uri)?;
+    let quote = parse(&response)?;
+
+    let text = textwrap::fill(quote.quote_text.trim(), 60);
+    let author = quote.quote_author.trim();
+
+    println!("\"{BLUE}{BOLD}{text}{RESET}\"");
+
+    if !author.is_empty() {
+        println!("{YELLOW}{author}{RESET}");
     }
+
+    Ok(())
+}
+
+fn parse(response: &str) -> Result<Quote> {
+    let fixed_response = response.replace("\\'", "'"); // i really hate this API
+
+    serde_json::from_str::<Quote>(&fixed_response).context("failed to serialize JSON")
+}
+
+fn get_request(uri: &str) -> Result<String> {
+    let mut response = ureq::get(uri).call().context("request error")?;
+    let string = response
+        .body_mut()
+        .read_to_string()
+        .context("failed to read response")?;
+
+    Ok(string)
 }
